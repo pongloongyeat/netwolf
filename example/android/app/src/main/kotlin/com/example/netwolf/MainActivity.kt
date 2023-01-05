af6e060e9ed92b20098a8bc1b0fb8080a2300e76
Loong Yeat <pongloongyeat@gmail.com>
2023-01-05 13:28:42 +0800

    Add example

---
+++ example/android/app/src/main/kotlin/com/example/netwolf/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.netwolf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:netwolf/netwolf.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      builder: NetwolfWidget.builder,
+      home: SomePage(),
+    ),
+  );
+}
+
+class SomePage extends StatelessWidget {
+  const SomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Netwolf Demo')),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Enabled: ${NetwolfController.instance.enabled}'),
+              const SizedBox(height: 16),
+              Wrap(
+                spacing: 16,
+                runSpacing: 16,
+                children: [
+                  ElevatedButton(
+                    onPressed: NetwolfController.instance.show,
+                    child: const Text('Show'),
+                  ),
+                  const ElevatedButton(
+                    onPressed: null,
+                    child: Text('Mock response'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => NetwolfController.instance
+                        .addResponse(_generateRandomResponse()),
+                    child: const Text('Add custom response'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  NetwolfResponse _generateRandomResponse() => NetwolfResponse(
+        url: Random().nextBool() ? 'https://google.com/' : null,
+        method: Random().nextBool()
+            ? HttpRequestMethod
+                .values[Random().nextInt(HttpRequestMethod.values.length)]
+            : null,
+        status: Random().nextBool()
+            ? HttpResponseStatus
+                .values[Random().nextInt(HttpResponseStatus.values.length)]
+            : null,
+      );
+}
