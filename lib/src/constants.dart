78212bc04d32d60f26f4dab04645614e9783d22f
Loong Yeat <pongloongyeat@gmail.com>
2023-01-28 20:04:08 +0800

    chore: Bump version to 0.1.0

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kPackageName = 'Netwolf';
-const kVersionName = '0.0.3';
+const kVersionName = '0.1.0';
 
 const kAnimationDuration = Duration(milliseconds: 300);
 const kAnimationCurveIn = Curves.easeIn;
