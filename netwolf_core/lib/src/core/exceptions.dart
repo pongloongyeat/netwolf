557824a36d5e2eb2bb344925852f4ee7a8c2b987
Pong Loong Yeat <pongloongyeat@gmail.com>
2023-08-25 18:34:05 +0800

    refactor: Remove netwolf_core and update tests

--- netwolf_core/lib/src/core/exceptions.dart
+++
@@ -1,5 +0,0 @@
-class NetwolfRecordNotFoundException implements Exception {}
-
-class NetwolfDecodingException implements Exception {}
-
-class NetwolfLoggingDisabledException implements Exception {}
--- netwolf_core/lib/src/models/result.dart
+++
@@ -1,9 +0,0 @@
-class Result<D, E> {
-  const Result(D this.data) : error = null;
-  const Result.error(E this.error, {this.data});
-
-  final D? data;
-  final E? error;
-
-  bool get hasError => error != null;
-}
